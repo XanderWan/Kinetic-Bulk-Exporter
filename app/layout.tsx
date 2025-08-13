d51a6632aee272acc752527ac87d0faa3873ee91
Xander Wanagel <xander.wanagel@gmail.com>
2025-08-13 13:02:20 -0700

    chore: rename app title to Kinetic Bulk Exporter

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Kinetic Bulk Exporter',
+  description: 'Bulk stitch backgrounds + demo + text + music, right in your browser',
   generator: 'v0.app',
 }
 
